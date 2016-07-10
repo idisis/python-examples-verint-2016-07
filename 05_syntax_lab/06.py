5500d5e3bc5b72cabac588861b4c85b7271e4307
idisis <nir.idisis@gmail.com>
2016-07-10 10:38:23 +0300

    Solution for: sol-python-verint.syntax-lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -1,9 +1,9 @@
-"""
-Write a program that randomizes 2 numbers
-and calculates their least common multiplier,
-that is the smallest number that is divisable
-by both.
-For example if the numbers were 4 and 6,
-program should print 12
-"""
+from random import randint
+num1 = randint(1, 10)
+num2 = randint(1, 10)
 
+print 'The numbers are: %d, %d.' %(num1, num2)
+for i in range(max(num1, num2), num1 * num2 + 1):
+  if i % num1 == 0 and i % num2 == 0:
+      print 'The result is %d.' %i
+      break
