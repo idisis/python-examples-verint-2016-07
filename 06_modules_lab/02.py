e6eecfbf05a570ec5b8ac7e2b7ec81861212ed02
idisis <nir.idisis@gmail.com>
2016-07-11 13:28:04 +0300

    Solution for: sol-python-verint.modules-lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -1,4 +1,12 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
-"""
+from sys import argv
 
+if len(argv) != 3:
+    print 'Usage:\n%s [a] [b]' %argv[0]
+    exit()
+
+try:
+  a = int(argv[1])
+  b = int(argv[2])
+  print 'The sum of %d and %d is %d.' %(a, b, a+b)
+except ValueError:
+  print 'The parameters \'a\' and \'b\' must be integers.'
