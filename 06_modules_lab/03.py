3aee227d01a015fd714ead9606dab25d018811ca
idisis <nir.idisis@gmail.com>
2016-07-11 13:28:05 +0300

    Solution for: sol-python-verint.modules-lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -1,11 +1,20 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
+import os
+import argparse
 
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
+parser = argparse.ArgumentParser(description='Delete large files recursively.')
+parser.add_argument('--target-dir', help='The target directory', default='.')
+parser.add_argument('--min-size', help='The minimum size of files to be deleted.', type=int, default=1024*1024)
+args = parser.parse_args()
 
-- Take threshold and path as command line arguments
-"""
-
+for root, dirs, filenames in os.walk(args.target_dir):
+    for name in filenames:
+        path = os.path.join(root, name)
+        if os.access(path, os.R_OK | os.W_OK):
+            file_stat = os.stat(path)
+            if file_stat.st_size >= args.min_size:
+                print 'would you like to delete \'%s\'? (Yes/no)' %path
+                answer = raw_input()
+                if answer == 'Yes':
+                    os.remove(path)
+        else:
+            print 'Access denied to file \'%s\'.' %path
