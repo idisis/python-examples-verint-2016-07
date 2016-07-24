953bf9ca4135fb24bc74a169cfcce1366dd25c3a
idisis <nir.idisis@gmail.com>
2016-07-24 19:56:39 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,19 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
-"""
+import argparse 
 
+parser = argparse.ArgumentParser(description='Combine two text files with interleave.')
+parser.add_argument('a', help='first file.')
+parser.add_argument('b', help='second file.')
+parser.add_argument('o', help='Output file.')
+args = parser.parse_args()
+
+with open(args.a, 'r') as a:
+  with open(args.b, 'r') as b:
+    with open(args.o, 'w') as o:
+        for line in a:
+            o.write(line)
+            if line[-1] != '\n':
+            	print >> o, ""
+        for line in b:
+            o.write(line)
+            if line[-1] != '\n':
+            	print >> o, ""
