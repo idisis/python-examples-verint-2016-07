18b4c753737ae3f2c505042832166745fcf6c9ff
idisis <nir.idisis@gmail.com>
2016-07-24 19:56:41 +0300

    Solution for: sol-python-verint.files-lab/02.py

--- 08_files_lab/02.py
+++ 08_files_lab/02.py
@@ -1,7 +1,18 @@
-"""
-Write a program that takes 3 names. The first two
-are existing file names and the last is a new file name
-Program should write the lines from the first two 
-files interwinded into the output file
-"""
+import argparse
+from itertools import izip_longest 
 
+parser = argparse.ArgumentParser(description='Combine two text files with interleave.')
+parser.add_argument('a', help='first file.')
+parser.add_argument('b', help='second file.')
+parser.add_argument('o', help='Output file.')
+args = parser.parse_args()
+
+with open(args.a, 'r') as a:
+  with open(args.b, 'r') as b:
+    with open(args.o, 'w') as o:
+        for line_tuple in izip_longest(a, b):
+            for line in line_tuple:
+                if line != None:
+                   o.write(line)
+                   if line[-1] != '\n':
+                     print >> o, ""
