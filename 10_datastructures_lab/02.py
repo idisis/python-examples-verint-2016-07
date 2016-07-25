11c10fa4651e45d1a4704cf060df7c03107d1d13
idisis <nir.idisis@gmail.com>
2016-07-25 13:39:04 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,5 +1,21 @@
-"""
-Write a program that prints only unique values
-from sys.argv (without program name)
-"""
+from sys import argv
+from sys import stdout
+from collections import defaultdict
+from collections import Counter
 
+filename = argv[1]
+words = defaultdict(list)
+
+# parse hosts file:
+with open(filename, 'r') as inputFile:
+    for line in inputFile:
+        if line[-1] == '\n':
+            line = line[:-1]
+        letterCount = Counter(sorted(line.lower()))
+        words[letterCount.__str__()].append(line)
+
+for list in words.itervalues():
+    for word in list:
+        stdout.write(word)
+        stdout.write(' ')
+    stdout.write('\n')
